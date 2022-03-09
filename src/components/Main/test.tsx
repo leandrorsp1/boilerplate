55df8a81fccc7083eaa6af0c017bf4e171e3e83f
leandrorsp1 <leandro.rodrigues@zesto.com.br>
2022-03-09 18:29:34 -0300

    tests running

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react'
+import Main from '.'
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<Main></Main>)
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild)
+  })
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main></Main>)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
+  })
+})
