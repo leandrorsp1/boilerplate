00620e9eb16dcf6dd68e21a03afa19ae579084c9
leandrorsp1 <leandro.rodrigues@zesto.com.br>
2022-03-14 10:15:51 -0300

    Finished boilerplate

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import Main from '.'
 describe('<Main />', () => {
   it('should render the heading', () => {
-    const { container } = render(<Main></Main>)
+    const { container } = render(<Main />)
     expect(
       screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
@@ -11,7 +11,7 @@ describe('<Main />', () => {
   })
 
   it('should render the colors correctly', () => {
-    const { container } = render(<Main></Main>)
+    const { container } = render(<Main />)
 
     expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
